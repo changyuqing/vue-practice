8159adaba275a472aca09d05e02c2a9db5c1a9eb
changyuqing <changyuqing_@outlook.com>
2018-04-08 22:52:56 +0800

    添加路由

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 <div class="container">
   <div class="row">
     <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-
+      <router-view></router-view>
     </div>
   </div>
 </div>
 </template>
 
 <script>
+
 export default {
 
   data () {
