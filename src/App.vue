a87d44519245f3f121d40a694c8a26461ebddefc
changyuqing <changyuqing_@outlook.com>
2018-04-09 14:22:52 +0800

    add the Header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="container">
+  <app-header></app-header>
   <div class="row">
     <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
       <router-view></router-view>
@@ -9,6 +10,7 @@
 </template>
 
 <script>
+import Header from './components/Header';
 
 export default {
 
@@ -16,10 +18,15 @@ export default {
     return {
 
     }
+  },
+  components:{
+    appHeader:Header
   }
 }
 </script>
 
 <style>
-
+body{
+  padding: 30px;
+}
 </style>
